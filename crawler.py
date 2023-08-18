6035329df515d68d3805c86e2d1b2b5b8de5d1a6
aminebenrejeb <benrejeb.amine0@gmail.com>
2023-08-18 10:51:40 +0200

    modify the crawler to get all job titles

--- crawler.py
+++ crawler.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request,urlopen
-url = "https://www.sfds.asso.fr/fr/n/506-consulter_les_offres_demploi/?jedu=MASTER&jcon=&jp=3"
+url = "https://www.sfds.asso.fr/fr/n/506- consulter_les_offres_demploi/?jedu=MASTER&jcon=&jp=3"
 page = urlopen(url)
 html = bs(page, 'html')
-print(html)
+#print(html)
+titre = html.findAll('td', {'class': 'jobLabel'})
+for element in titre: print(element.get_text())
