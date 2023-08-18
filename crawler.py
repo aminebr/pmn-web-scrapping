c0d903203aeb2b26c7e62735b2d0af253e9786da
aminebenrejeb <benrejeb.amine0@gmail.com>
2023-08-18 15:32:31 +0200

    Finished Project

--- crawler.py
+++ crawler.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request,urlopen
+
+
+
+
+
+
 url = "https://www.sfds.asso.fr/fr/n/506-consulter_les_offres_demploi/?jedu=MASTER&jcon=&jp=3"
 page = urlopen(url)
 html = bs(page, 'html')
@@ -26,4 +32,21 @@ for job in allJobs:
 #Print all Titles Jobs
 print(titre_offre)
 #Print all Date Pub
-print(date_offre) #Print all Resume print(resume_offre)
+print(date_offre)
+#Print all Resume
+print(resume_offre)
+
+
+import pandas as pd
+dfJobs = pd.DataFrame({ 'TITRE_OFFRE': titre_offre, 'DATE_PUB_OFFRE': date_offre, 'RESUME_OFFRE': resume_offre})
+
+
+from sqlalchemy import create_engine
+# create sqlalchemy engine
+user = 'root'
+passw = 'supersecret'
+host = 'localhost' # either localhost or ip e.g. '172.17.0.2' or hostname address
+port = 3307
+database = 'jobs_scraping'
+engine = create_engine('mysql+pymysql://' + user + ':' + passw + '@' + host + ':' + str(port) + '/' + database , echo=False)
+dfJobs.to_sql('jobs', con = engine, if_exists = 'append',index= False, chunksize = 1000)
---
+++ mysql/sql-scripts/jobs.sql
@@ -0,0 +1,7 @@
+Create Database jobs_scraping;
+use jobs_scraping;
+CREATE TABLE jobs (
+TITRE_OFFRE varchar(25),
+DATE_PUB_OFFRE  varchar(25),
+RESUME_OFFRE varchar(100)
+);
